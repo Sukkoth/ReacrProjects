8b84d5d25449af615d4329cc5a42ee460559f1cb
Sukkoth <suukootj@gmail.com>
2023-06-20 23:04:59 +0300

    added data state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import './App.css'
+import { useState } from 'react'
 import dataSet from './data' //this is an array
 import Question from './Question' // this one is component
+import './App.css'
+
 function App() {
+  const [data, setData] = useState(dataSet)
   return (
     <main className='main'>
       <div className='container'>
         <div className="header">Questions And Answers About Things</div>
         <section className="questions">
           {
-            dataSet.map((question) => (
+            data.map((question) => (
              <Question question={question} key={question.id}/>
             ))
           }
         </section>
         {
-          dataSet.length == 0 && <div className='card'><p style={{color: 'red'}}>No Question Data Found</p></div>
+          data.length == 0 && <div className='card'><p style={{color: 'red'}}>No Question Data Found</p></div>
         }
       </div>
     </main>
