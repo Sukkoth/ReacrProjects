b1300dd1c7cfdb51bea0db6b19fed5eeeab6bdea
Sukkoth <suukootj@gmail.com>
2023-06-20 23:09:25 +0300

    Formatted with Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import dataSet from './data' //this is an array
-import Question from './Question' // this one is component
-import './App.css'
+import { useState } from 'react';
+import dataSet from './data'; //this is an array
+import Question from './Question'; // this one is component
+import './App.css';
 
 function App() {
-  const [data, setData] = useState(dataSet)
-  return (
-    <main className='main'>
-      <div className='container'>
-        <div className="header">Questions And Answers About Things</div>
-        <section className="questions">
-          {
-            data.map((question) => (
-             <Question question={question} key={question.id}/>
-            ))
-          }
-        </section>
-        {
-          data.length == 0 && <div className='card'><p style={{color: 'red'}}>No Question Data Found</p></div>
-        }
-      </div>
-    </main>
-  )
+    const [data, setData] = useState(dataSet);
+    return (
+        <main className='main'>
+            <div className='container'>
+                <div className='header'>Questions And Answers About Things</div>
+                <section className='questions'>
+                    {data.map((question) => (
+                        <Question question={question} key={question.id} />
+                    ))}
+                </section>
+                {data.length == 0 && (
+                    <div className='card'>
+                        <p style={{ color: 'red' }}>No Question Data Found</p>
+                    </div>
+                )}
+            </div>
+        </main>
+    );
 }
 
-export default App
+export default App;
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-const Question = ({question}) => {
-    const [showAnswer, setShowAnswer] = useState(false)
-  return (
-    <div className="card" key={question.id}>
-    <div className="question">
-      <p>{question.question}</p>
-      <i className={`fa-solid ${showAnswer ? 'fa-minus' : 'fa-plus'}`} onClick={()=>setShowAnswer(!showAnswer)}></i>
-    </div>
-    {
-        showAnswer && <p className='answer'>{question.answer}</p> }
-  </div>
-  )
-}
+const Question = ({ question }) => {
+    const [showAnswer, setShowAnswer] = useState(false);
+    return (
+        <div className='card' key={question.id}>
+            <div className='question'>
+                <p>{question.question}</p>
+                <i
+                    className={`fa-solid ${
+                        showAnswer ? 'fa-minus' : 'fa-plus'
+                    }`}
+                    onClick={() => setShowAnswer(!showAnswer)}
+                ></i>
+            </div>
+            {showAnswer && <p className='answer'>{question.answer}</p>}
+        </div>
+    );
+};
 
-export default Question
+export default Question;
