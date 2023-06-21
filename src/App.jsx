985c37948d452ff9f36e0063bdb8fe579cece001
Sukkoth <Suukootj@gmail.com>
2023-06-21 16:26:13 +0300

    Main layouts, modify ul later

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
 function App() {
-    return <h1>TABS</h1>;
+    return (
+        <main className='container'>
+            <div className='navigation'>
+                <button className='navButton'>Tommy</button>
+                <button className='navButton'>BIGDROP</button>
+                <button className='navButton'>CUKER</button>
+            </div>
+            <div className='content'>
+                <h3>Full Stack Web Developer</h3>
+                <p className='company'>Tommy</p>
+                <p className='date'>December 2015 - present</p>
+                <ul className='duties'>
+                    <li>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Perspiciatis quibusdam optio, illo cupiditate quas
+                        eveniet! Hic pariatur ipsam debitis omnis?
+                    </li>
+                    <li>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Perspiciatis quibusdam optio, illo cupiditate quas
+                        eveniet! Hic pariatur ipsam debitis omnis?
+                    </li>
+                    <li>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Perspiciatis quibusdam optio, illo cupiditate quas
+                        eveniet! Hic pariatur ipsam debitis omnis?
+                    </li>
+                </ul>
+            </div>
+        </main>
+    );
 }
 
 export default App;
