b5cb864b8813656cb825cdb51fb7ec98cb2f6a68
Sukkoth <Suukootj@gmail.com>
2023-06-21 15:32:22 +0300

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
-import { useState } from 'react';
-import data from './data';
-import Menu from './Menu';
-import Filters from './Filters';
-import Header from './Header';
-
 function App() {
-    const [foods, setFoods] = useState(data);
-
-    const onFilter = (category = null) => {
-        if (!category) return setFoods(data);
-        return setFoods(() =>
-            data.filter((item) => item.category === category)
-        );
-    };
-
-    const filters = [];
-    data.forEach(
-        (item) =>
-            !filters.includes(item.category) && filters.push(item.category)
-    );
-    return (
-        <main className='container'>
-            <Header />
-            <Filters onFilter={onFilter} filters={filters} />
-            <Menu foods={foods} />
-        </main>
-    );
+    return <h1>TABS</h1>;
 }
 
 export default App;
